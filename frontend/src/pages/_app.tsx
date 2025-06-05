806e127421debdc9a8ef19270be0fbdf2f742ede
Lucas Oliveira <olivmath@protonmail.com>
2025-06-05 16:08:35 -0300

    all good

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import '../../app/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+import "../../app/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider, type Locale } from '@rainbow-me/rainbowkit';
-import { config } from '@/config/wagmi';
-
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import {
+  ConnectButton,
+  darkTheme,
+  RainbowKitProvider,
+  type Locale,
+} from "@rainbow-me/rainbowkit";
+import { config } from "@/config/wagmi";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +21,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider locale={locale}>
+        <RainbowKitProvider
+          locale={locale}
+          theme={darkTheme({
+            accentColor: "#6366f1",
+            accentColorForeground: "white",
+            borderRadius: "large",
+            fontStack: "system",
+          })}
+        >
           <Component {...pageProps} />
+          <ConnectButton />
+          <Toaster theme="dark" position="top-right" richColors />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,14 +3,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
+    <div>
     </div>
   );
 };
